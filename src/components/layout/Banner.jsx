b1cd7c454383236563e75a536ad969537219615d
codayon <codayon@proton.me>
2025-09-16 12:12:18 +0600

    refactor: add banner with code

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -1,14 +1,34 @@
+import Button from "../common/Button";
+import Container from "../common/Container";
+import Flex from "../common/Flex";
 import ImageWrapper from "../common/ImageWrapper";
-// import BannerSrc from "../../assets/images/banner.png";
+import Information from "./Information";
 
 const Banner = () => {
   return (
-    <section>
-      <ImageWrapper
-        src="https://res.cloudinary.com/droh6dnbd/image/upload/v1757569962/banner_qilsva.png"
-        alt="Website Banner"
-        className={"cursor-pointer aspect-[1920/677]"}
-      />
+    <section className="bg-snow">
+      <Container>
+        <Flex className="justify-between py-16">
+          <div className="">
+            <h1 className="font-bold text-5xl">Final Offer</h1>
+            <p className="text-stone flex items-center py-8">
+              Up to
+              <span className="text-onyx font-bold text-4xl px-2">50%</span>sale
+              for all furniture items!
+            </p>
+            <Button
+              className="inline-block"
+              label="Shop Now"
+            />
+          </div>
+          <ImageWrapper
+            src="https://res.cloudinary.com/droh6dnbd/image/upload/v1758001214/headphone_ext6ps.png"
+            alt="headphone_ext6ps.png"
+            className="bg-transparent aspect-500/500"
+          />
+        </Flex>
+      </Container>
+      <Information />
     </section>
   );
 };
