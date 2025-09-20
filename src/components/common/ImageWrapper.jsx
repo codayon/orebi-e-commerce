e6c4c1592934c9bf5af5f6507a585a2597a237ba
codayon <codayon@proton.me>
2025-09-20 15:10:30 +0600

    refactor: write banner ad seciton with code

--- src/components/common/ImageWrapper.jsx
+++ src/components/common/ImageWrapper.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const ImageWrapper = ({
@@ -7,30 +6,21 @@ const ImageWrapper = ({
   className = "",
   imgClassName = "",
 }) => {
-  const [loaded, setLoaded] = useState(false);
-
   return (
     <div
       className={twMerge(
-        "relative overflow-hidden bg-gray-200 flex items-center justify-center select-none",
+        "relative overflow-hidden bg-fog flex items-center justify-center select-none",
         className
       )}
     >
-      {!loaded && (
-        <div className="absolute inset-0 animate-pulse bg-gray-300" />
-      )}
-      {src && (
-        <img
-          src={src}
-          alt={alt}
-          onLoad={() => setLoaded(true)}
-          className={twMerge(
-            "w-full h-full object-cover transition-opacity duration-500",
-            imgClassName,
-            loaded ? "opacity-100" : "opacity-0"
-          )}
-        />
-      )}
+      <img
+        src={src}
+        alt={alt}
+        className={twMerge(
+          "w-full h-full object-cover transition-opacity duration-500",
+          imgClassName
+        )}
+      />
     </div>
   );
 };
--- src/components/layout/BannerAd.jsx
+++ src/components/layout/BannerAd.jsx
@@ -1,3 +1,5 @@
+import Flex from "../common/Flex";
+import Button from "../common/Button";
 import Container from "../common/Container";
 import ImageWrapper from "../common/ImageWrapper";
 
@@ -5,11 +7,22 @@ const BannerAd = () => {
   return (
     <section className="py-12">
       <Container>
-        <ImageWrapper
-          src="https://res.cloudinary.com/droh6dnbd/image/upload/v1758009944/ad-4_h8uypk.webp"
-          alt="AdImg"
-          className={"cursor-pointer"}
-        />
+        <Flex className="bg-fog ">
+          <ImageWrapper
+            src="https://res.cloudinary.com/droh6dnbd/image/upload/v1758345210/clock_lr26je.webp"
+            alt="clock"
+            className="md:px-44 md:h-80"
+            imgClassName="md:scale-110"
+          />
+          <div className="flex flex-col gap-6 py-16">
+            <h3 className="font-bold text-4xl">Phone of the year</h3>
+            <p className="leading-7 max-w-lg">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry orem Ipsum..
+            </p>
+            <Button label="Shop Now" className="self-start" />
+          </div>
+        </Flex>
       </Container>
     </section>
   );
